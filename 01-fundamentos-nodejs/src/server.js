74a1ec0a1bce5cc0700a8bf03705e70ad3b021b3
EaeDave <d4vd.sv@gmail.com>
2025-10-14 20:45:34 -0300

    Rotas de criação e listagem (Métodos HTTP)

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -5,9 +5,38 @@
 import http from 'node:http';
 
 // * Aplicações HTTP => APIs
+/**
+ * * Rotas comuns (CRUD)
+ * * - Criar usuários (Método HTTP POST)
+ * * - Listar usuários (Método HTTP GET)
+ * * - Editar de usuários (Método HTTP PATCH OU PUT)
+ * * - Remover usuários (Método HTTP DELETE)
+ */
+
+// * Os métodos são mais semânticos
+// * GET => Buscar um recurso do back-end
+// * POST => Criar um recurso no back-end
+// * PUT => Editar ou atualizar um recurso no back-end
+// * PATCH => Atualizar uma informação específica de um recurso no back-end
+// * DELETE => Deletar um recurso do back-end
+
 const server = http.createServer((req, res) => {
+	const { method, url } = req; // req.method e req.url utilizando desestruturação de objetos {}
+
+	// Early return
+	// * Criando um endpoint /users para o método GET
+	if (method === 'GET' && url === '/users') {
+		return res.end('Listagem de usuários');
+	}
+
+	// * Criando um endpoint /users para o método POST
+	if (method === 'POST' && url === '/users') {
+		return res.end('Criação de usuários');
+	}
+	console.log(method, url);
+
 	return res.end('Olá, Mundo');
 });
 
-console.log('Iniciando server...');
+console.log('Servidor escutando...');
 server.listen(3333);
