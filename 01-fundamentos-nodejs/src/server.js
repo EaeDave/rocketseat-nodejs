31227724f131c23e7156f86d7915846d476206d1
EaeDave <d4vd.sv@gmail.com>
2025-10-14 20:01:49 -0300

    Criando um projeto Node.js

---
+++ 01-fundamentos-nodejs/src/server.js
@@ -0,0 +1,12 @@
+// Por padrão os imports do Node utiliza-se CommonJS => Require
+// A forma mais moderna é usando o ESModules => import/export
+// Para utilizar ESModules é preciso acrescentar no package.json "type": "module",
+// ! const http = require('http'); // ! CommonJS => require
+import http from 'node:http';
+
+// * Aplicações HTTP => APIs
+const server = http.createServer((req, res) => {
+	return res.end('Hello World');
+});
+
+server.listen(3333);
