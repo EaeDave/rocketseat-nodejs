b659505764875943f9bd0fd331c6b099270ba22a
EaeDave <d4vd.sv@gmail.com>
2025-10-16 19:15:55 -0300

    Entendendo Streams no Node

---
+++ 01-fundamentos-nodejs/streams/fundamentals.js
@@ -0,0 +1,12 @@
+// Exemplos de streaming:
+// Netflix & Spotify
+
+// * Conceito - Processar arquivos/entidades progressivamente e já manipular o que já foi processado
+
+// Importação de clientes via CSV (Excel)
+// 1gb - 1.000.000 linhas
+// POST /upload import.csv
+
+// 10mb/s - 100s
+
+// * Readable Streams / Writable Streams
