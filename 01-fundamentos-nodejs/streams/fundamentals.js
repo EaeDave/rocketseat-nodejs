59faacfdd9042a3680dabb37e2dab3fd318da11e
EaeDave <d4vd.sv@gmail.com>
2025-10-16 20:02:22 -0300

    Criando stream de leitura

--- 01-fundamentos-nodejs/streams/fundamentals.js
+++ 01-fundamentos-nodejs/streams/fundamentals.js
@@ -10,3 +10,28 @@
 // 10mb/s - 100s
 
 // * Readable Streams / Writable Streams
+
+// Streams ->
+// * Tudo que estou recebendo como entrada: process.stdin
+// * Eu estou encaminhando para a saída: .pipe(process.stdout)
+// process.stdin.pipe(process.stdout);
+
+import { Readable } from 'node:stream';
+
+class oneToHundredStream extends Readable {
+	index = 1;
+	_read() {
+		const i = this.index++;
+
+		setTimeout(() => {
+			if (i > 100) {
+				this.push(null);
+			} else {
+				const buf = Buffer.from(String(i));
+				this.push(buf);
+			}
+		}, 1000);
+	}
+}
+
+new oneToHundredStream().pipe(process.stdout);
